afb82a0e8196de02f19f825e3a5fcd16f6bd0e79
Relativiteit <alejocain@gmail.com>
2021-08-07 15:56:31 +0200

    Basics Java Streams

--- src/streams/Main.java
+++ src/streams/Main.java
@@ -1,8 +1,6 @@
 package streams;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.*;
 
 public class Main {
@@ -35,15 +33,47 @@ public class Main {
                 .sorted(Comparator.comparing(Person::getAge).thenComparing(Person::getGender).reversed())
                 .collect(Collectors.toList());
 
-        sorted.forEach(System.out::println);
+//        sorted.forEach(System.out::println);
         // All match
+        boolean allMatch = people.stream()
+                .allMatch(person -> person.getAge() > 5);
+//        System.out.println(allMatch);
         // Any match
+        boolean anyMatch = people.stream()
+                .anyMatch(person -> person.getAge() > 121);
+//        System.out.println((anyMatch));
         // None match
+        boolean noneMatch = people.stream()
+                .noneMatch(person -> person.getName().equals("Antonio"));
+//        System.out.println(noneMatch);
         // Max
+        people.stream()
+                .max(Comparator.comparing(Person::getAge));
+//                .ifPresent(System.out::println);
+
         // Min
+        people.stream()
+                .min(Comparator.comparing(Person::getAge));
+//                .ifPresent(System.out::println);
         // Group
-        // constructor
+        Map<Gender, List<Person>> groupByGender = people.stream()
+                .collect(Collectors.groupingBy(Person::getGender));
+//
+//
+//        groupByGender.forEach((gender, people1) -> {
+//            System.out.println(gender);
+//            people1.forEach(System.out::println);
+//            System.out.println();
+//        });
+
+        Optional<String> oldestFemaleAge = people.stream()
+                .filter(person -> person.getGender().equals(Gender.FEMALE))
+                .max(Comparator.comparing(Person::getAge))
+                .map(Person::getName);
+
+        oldestFemaleAge.ifPresent(System.out::println);
     }
+    // constructor
     private static List<Person> getPeople() {
         return List.of(
                 new Person("James Bond", 20, Gender.MALE),
@@ -52,7 +82,7 @@ public class Main {
                 new Person("Alex Boz", 14, Gender.MALE),
                 new Person("Jamie Goa", 99, Gender.MALE),
                 new Person("Anna Cook", 7, Gender.FEMALE),
-                new Person("Zelda Brown", 120, Gender.MALE)
+                new Person("Zelda Brown", 120, Gender.FEMALE)
         );
     }
 }

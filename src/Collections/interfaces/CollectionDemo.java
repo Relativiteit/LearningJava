911efd1830a10c6f5ae25270c888028e896bfc39
Relativiteit <alejocain@gmail.com>
2021-08-23 11:42:30 +0200

    Collections

--- src/Collections/interfaces/CollectionDemo.java
+++ src/Collections/interfaces/CollectionDemo.java
@@ -1,7 +1,9 @@
 package Collections.interfaces;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Iterator;
 
 public class CollectionDemo {
     public static void main(String[] args)
@@ -16,7 +18,23 @@ public class CollectionDemo {
         System.out.println(bikeCollection);
         // you can see the available API syntax: collection name.
 
-        bikeCollection.
+        bikeCollection.forEach((element) -> {
+            System.out.println(element);
+        });
+        bikeCollection.stream().iterator();
+
+
+        System.out.println(bikeCollection);
+        // print size of array
+        System.out.println(bikeCollection.size());
+        // to array wrapped in arrays to string method
+        System.out.println(Arrays.toString(bikeCollection.stream().toArray()));
+
+        System.out.println(bikeCollection.isEmpty());
+        bikeCollection.clear();
+        System.out.println(bikeCollection.isEmpty());
+
+
 
     }
 }

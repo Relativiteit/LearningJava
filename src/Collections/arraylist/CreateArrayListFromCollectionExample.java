31ed55538d37353da22a7634b62b8209d0f79c69
Relativiteit <alejocain@gmail.com>
2021-08-23 13:40:59 +0200

    Arraylist manipulation

---
+++ src/Collections/arraylist/CreateArrayListFromCollectionExample.java
@@ -0,0 +1,41 @@
+package Collections.arraylist;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateArrayListFromCollectionExample {
+    public static void main (String[] args)
+    {
+        // create arrayList object
+        List<Integer> firstFivePrimeNumbers = new ArrayList<>();
+        firstFivePrimeNumbers.add(2);
+        firstFivePrimeNumbers.add(3);
+        firstFivePrimeNumbers.add(5);
+        firstFivePrimeNumbers.add(7);
+        firstFivePrimeNumbers.add(11);
+        System.out.println(firstFivePrimeNumbers);
+
+        // create array list object
+        List<Integer> firstTenPrimeNumbers = new ArrayList<>(firstFivePrimeNumbers);
+        System.out.println(firstTenPrimeNumbers);
+
+        // create array list object
+        List<Integer> nextFivePrimeNumbers = new ArrayList<>();
+        nextFivePrimeNumbers.add(13);
+        nextFivePrimeNumbers.add(17);
+        nextFivePrimeNumbers.add(19);
+        nextFivePrimeNumbers.add(23);
+        nextFivePrimeNumbers.add(29);
+        System.out.println((nextFivePrimeNumbers));
+
+        firstTenPrimeNumbers.addAll(nextFivePrimeNumbers);
+        System.out.println(firstTenPrimeNumbers);
+
+
+
+
+
+
+    }
+}

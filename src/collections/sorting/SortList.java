83c608c82707a5b581c39a8341f96007b1ec9e16
Relativiteit <alejocain@gmail.com>
2021-08-25 08:21:30 +0200

    Added collections

--- src/collections/sorting/SortList.java
+++ src/collections/sorting/SortList.java
@@ -1,6 +1,7 @@
 package collections.sorting;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -26,13 +27,31 @@ public class SortList {
         employees.add(new Employee(30, "Bas", 25, 55000));
         employees.add(new Employee(40, "Neville", 27, 320000));
 
-       /* Collections.sort(employees, new Mysort()); // ascending order
+        Collections.sort(employees, new Comparator<Employee>() {
+            @Override
+            public int compare(Employee o1, Employee o2) {
+                return (int) (o2.getSalary() -o1.getSalary()));
+            }
+        });
+
+
+
+
+        Collections.sort(employees, new Mysort()); // ascending order
         System.out.println(employees);
 
         Collections.sort(employees, new Mysort()); // descending order
-        System.out.println(employees); */
+        System.out.println(employees);
+
+
+
+
+        (01, 02) ()->(int) (o2.getSalary()- o1.getSalary());
+        Collections.sort(employees, );
+
     }
 }
+
 
 class Mysort implements Comparator<Employee> {
 

c656a13814691774d30a567d17cb233f58df7679
Relativiteit <alejocain@gmail.com>
2021-09-01 21:12:36 +0200

    Object creation

--- src/chapter4/ConstructorTest.java
+++ src/chapter4/ConstructorTest.java
@@ -1,8 +1,73 @@
 package chapter4;
+import java.util.*;
+import java.util.Random;
 
 public class ConstructorTest {
     public static void main(String[] args)
     {
-        var staff = new Employee[3];
+        var staff = new MemberInEmployment[3];
+
+        staff[0] = new MemberInEmployment("Harry", 4000);
+        staff[1] = new MemberInEmployment(60000);
+        staff[2] = new MemberInEmployment();
+
+        // print out information about all Employee objects
+        for (MemberInEmployment e : staff)
+            System.out.println("name= " + e.getName() + "id= " + e.getId() + "salary= " + e.getSalary());
+    }
+}
+
+class MemberInEmployment
+{
+    private static int nextId;
+    private int id;
+    private String name =""; // instance field initialization
+    private double salary;
+
+    // static initialization block
+    static
+    {
+        var generator = new Random();
+        // set nextId to a random number between 0 and 9999
+        nextId = generator.nextInt(10000);
+    }
+
+    // object initialiazation block
+    {
+        id = nextId;
+        nextId++;
+    }
+
+    // three overloaded constructors public Employee(String n, double s)
+    public MemberInEmployment(String n, double s)
+    {
+        name = n;
+        salary = s;
+    }
+    public MemberInEmployment(double s)
+    {
+        // calls the Employee(String, double) constructor
+        this("Employee # " + nextId, s);
+    }
+
+    // the default constructor
+    public MemberInEmployment()
+    {
+        // name initialized to ""--see above
+        // salary not explicitly set--initialized to 0
+        // id initilized in initalization block
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
     }
 }
--- src/chapter5/enumeration/EnumTest.java
+++ src/chapter5/enumeration/EnumTest.java
@@ -1,10 +1,34 @@
 package chapter5.enumeration;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.*;
+
 public class EnumTest {
 
     public static void main(String[] main)
     // logic down here
     {
-
+        var in = new Scanner(System.in);
+        System.out.print("enter a size: (SMALL, MEDIUM, LARGE, EXTRA_LARGE)");
+        String input = in.next().toUpperCase();
+        // System.out.println(in.next());   way to print input fast
+        Size size = Enum.valueOf(Size.class, input);
+        System.out.println("size= " + size);
+        System.out.println("abbreviation=" + size.getAbbreviation());
+        if (size == Size.EXTRA_LARGE)
+            System.out.println("Good job HUlK you payed attention to the sizes.");
     }
+
+}
+
+enum Size
+{
+    SMALL("S"), MEDIUM("M"), LARGE("L"), EXTRA_LARGE("XL");
+
+    private Size(String abbreviation) { this.abbreviation = abbreviation;}
+    public String getAbbreviation() { return abbreviation; }
+
+    private String abbreviation;
+
 }
